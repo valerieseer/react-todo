c12cde7e9d4285b12f7e229bd8ac1c952f9c128a
valerieseer <seervalerie@gmail.com>
2017-05-25 20:45:45 -0500

    event handling for submit button

--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,35 @@ const ReactDom = require("react-dom")
 
 //Todo component
 class TodoComponent extends React.Component {
-   render() {
+	constructor(props) {
+		super(props)
+		this.state = {
+			items: []
+		}
+		this.handleButtonClick = this.handleButtonClick.bind(this)
+	}
+	handleButtonClick(event) {
+		event.preventDefault();
+		let todoInput = document.getElementsByClassName("todo-input")[0].value;
+		this.setState(function(prevState) {
+			return {
+				items: prevState.items.push(todoInput)
+			}
+		})
+	}
+   	render() {
+   		console.log(this.state)
+   		let list = this.state.items.map(function(element) {
+   			return <li>{element}<button></button></li>
+   		})
 		return (
-			<div class="todo-list">
-				<ul></ul>
+			<div className="todo-list">
+				<p>What should I do today:</p>
+				<form onClick={this.handleButtonClick}>
+					<input type="text" className="todo-input"/>
+					<input type="submit" value="Add"/>
+				</form>
+				<ul>{list}</ul>
 			</div>
 		)
 	}
