308f30993fd1d3f8da6e6b1af8c4b8f6fcce8298
Valerie Seer <seervalerie@gmail.com>
2017-07-10 10:21:09 -0500

    cleaned up code

--- src/app/index.js
+++ src/app/index.js
@@ -1,35 +1,8 @@
-const React = require("react")
-const ReactDom = require("react-dom")
-require("./css/index.css")
+const React = require("react");
+const ReactDom = require("react-dom");
+require("./css/index.css");
 
-/******* TO DO ITEM *******/
-// class TodoItems extends React.Component {
-// 	render() {
-// 		let todoItem = this.props.todo
-//
-// 		function createTodo () {
-// 			return <li key={todos.key}>{todos.text}</li>
-// 		}
-//
-// 		let listTodos = todoItem.map(createTodo)
-//
-// 		return (
-// 			<ul>
-// 			 {listTodos}
-// 		 	</ul>
-// 		)
-// 	}
-// }
-
-// functional stateless component
-function Todo(props) {
-	return (
-		<li><button className="delete-todo" onClick={(e) => props.deleteTodo(e, props.index)} data-index={props.index}> &times; </button>
-		<button type='button'>E</button>{props.el}</li>
-	)
-}
-
-class Todo1 extends React.Component {
+class TodoItem extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -49,10 +22,10 @@ class Todo1 extends React.Component {
 		return (
 			<div>
 				<input onClick={(e) => this.props.handleCheck(e, this.props.idx)} type='checkbox'/>
-				<input  
+				<input
 						value={this.state.stuff}
-						onChange={this.handleChange}/>	
-				<button onClick={(e) => this.props.deleteTodo(e, this.props.idx)} type='button'>&times;</button>							
+						onChange={this.handleChange}/>
+				<button onClick={(e) => this.props.deleteTodo(e, this.props.idx)} type='button'>&times;</button>
 			</div>
 		)
 	}
@@ -77,7 +50,7 @@ class TodoApp extends React.Component {
 		let todoInput = document.getElementsByClassName("todo-input")[0].value;
  		this.setState({
 			todos: [...this.state.todos, todoInput]
-		},() => console.log(this.state.todos))
+		})
 	}
 
 	deleteTodo(e, idx) {
@@ -96,11 +69,11 @@ class TodoApp extends React.Component {
 		if(e.target.checked) {
 			let todos = this.state.todos;
 			let newArr = todos.slice(0, idx).concat(todos.slice(idx+1));
-			let completedItem = todos[idx];	
+			let completedItem = todos[idx];
 			this.setState({
 				todos: newArr,
 				completed: [...this.state.completed, completedItem]
-			}, () => console.log('new completed state is', this.state.completed));					
+			}, () => console.log('new completed state is', this.state.completed));
 		}
 	}
 
@@ -112,17 +85,16 @@ class TodoApp extends React.Component {
 					<button type="submit" value="add">add</button>
 				</form>
 				{this.state.todos.map(function(el, index) {
- 				return <Todo1 deleteTodo={this.deleteTodo}
+ 				return <TodoItem deleteTodo={this.deleteTodo}
 				 			  handleCheck={this.handleCheck}
 				 			  parentValue={el}
-				 			  doggy={el}
 							  idx={index}/>
 				}.bind(this))}
 
 				<div>
 					<h1>Completed</h1>
 					{this.state.completed.map(function(el, index) {
-						return <Todo1 parentValue={el}/>
+						return <TodoItem parentValue={el}/>
 					}.bind(this))}
 				</div>
 			</div>
